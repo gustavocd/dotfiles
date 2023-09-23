77bca9aa3162a63b3dd0e658babf49e65e7c23f0
Gustavo Castillo <gcdcoder@gmail.com>
2023-09-22 22:50:29 -0600

    Remove lua ls and update plugins file

--- nvim/.config/nvim/lua/gustavocd/lsp/settings/init.lua
+++ nvim/.config/nvim/lua/gustavocd/lsp/settings/init.lua
@@ -3,10 +3,8 @@ if not status_ok then
 	return
 end
 
-local luals = require("lua.gustavocd.lsp.settings.luals")
 local jsonls = require("gustavocd.lsp.settings.jsonls")
 local pyright = require("gustavocd.lsp.settings.pyright")
 
-lsp_config.lua_ls.setup(luals)
 lsp_config.jsonls.setup(jsonls)
 lsp_config.pyright.setup(pyright)
